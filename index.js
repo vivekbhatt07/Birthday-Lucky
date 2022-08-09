bf4591fab7d574b758c89d8a78a833f878a0c9ae
Vivek Bhatt <vivekbhatt618@gmail.com>
2022-08-10 00:19:03 +0530

    Add: reference in js

---
+++ index.js
@@ -0,0 +1,4 @@
+const dobInput = document.querySelector("#dob_input");
+const luckyInput = document.querySelector("#lucky_input");
+const checkBtn = document.querySelector("#check_btn");
+const msg = document.querySelector(".message");
